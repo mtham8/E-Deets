bcb4b6e60e21a85229bc3d3ee385c8cea05e0937
Michelle <mtham101@gmail.com>
2016-06-20 10:30:44 -0700

    Edit package.json

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      visuals: []
+    }
+  }
+
   render() {
     return (
       <div>E-Deets Search</div>
